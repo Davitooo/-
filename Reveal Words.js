3d2419810c431ac5befffd959dca8548d263a37a
David Georgiev <114015427+Davitooo@users.noreply.github.com>
2022-11-11 18:56:20 +0200

    Reveal Words.js

---
+++ Reveal Words.js
@@ -0,0 +1,12 @@
+function stringDemo(words, text) {
+        text = ` ${text}`;
+    const wordsArray = words.split(', ');
+    wordsArray.forEach(word => {
+        const match = ` ${'*'.repeat(word.length)}`;
+        text = text.replace(match, ` ${word}`);
+    });
+
+    console.log(text.trim());
+}
+stringDemo('great, learning',
+'softuni is ***** place for ********new programming languages');
